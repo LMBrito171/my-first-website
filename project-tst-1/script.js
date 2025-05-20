fbdd482d341f98bd0dc335aab3f895dc9e8dd74b
Lucca <lmbrito171@icloud.com>
2025-05-20 03:40:51 -0300

    blog posts update, now they are dynamical via JS

--- project-tst-1/script.js
+++ project-tst-1/script.js
@@ -30,4 +30,39 @@ function sayHello() {
       charCount.textContent = `characters left: ${remaining}`;
     })
   }
+
   
+    const blogPosts = [{
+      title: "How I built my first Website",
+      date: "May 20th, 2025",
+      summary: "this is a short summary of the article(...)",
+      link: "#"
+    },
+    {
+      title: "Dark Mode with JS",
+      date: "May 19th, 2025",
+      summary: "Dark mode isn’t just cool — it’s a great practice for accessibility and user comfort. Here's how I made it work persistently.",
+      link: "#"
+
+    }
+  ];
+  function renderBlogPosts () {
+    const container = document.getElementById("blog-posts");
+    if (!container) return;
+
+    blogPosts.forEach(post => {
+      const article = document.createElement("article");
+      article.className = "blog-post";
+
+      article.innerHTML= `
+        <h2>${post.title}</h2>
+        <p><small>Posted on ${post.date}</small></p>
+        <p>${post.summary}</p>
+        <a href="${post.link}">Read More</a>
+      `;
+
+      container.appendChild(article);
+    });
+  }
+
+  renderBlogPosts();
