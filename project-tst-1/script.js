1940064837fec015c497a9aaad7b9dc7735c74a4
Lucca <lmbrito171@icloud.com>
2025-05-20 01:00:38 -0300

    Dark mode is now persistent across pages and headers are the correct color

--- project-tst-1/script.js
+++ project-tst-1/script.js
@@ -2,8 +2,20 @@ function sayHello() {
     alert('You clicked the button! Welcome to your first site.');
   }
   
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+  
   const button = document.getElementById("toggle-theme");
-
+  
   button.addEventListener("click", () => {
+    // Toggle the dark class
     document.body.classList.toggle("dark");
+  
+    // Save the current theme to localStorage
+    if (document.body.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
   });
