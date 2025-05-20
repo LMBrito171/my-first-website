f3e4bcf97919ba00d4f2c44f3ccf4226fd4d9c50
Lucca <lmbrito171@icloud.com>
2025-05-20 00:44:37 -0300

    Added dark mode(non-consistent across site)

--- project-tst-1/script.js
+++ project-tst-1/script.js
@@ -2,3 +2,8 @@ function sayHello() {
     alert('You clicked the button! Welcome to your first site.');
   }
   
+  const button = document.getElementById("toggle-theme");
+
+  button.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+  });
